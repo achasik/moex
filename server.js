fd84bffcd053d92a7f276cb901f4da5c9fe5b808
Андрей Часовской <sp-admin-dev@SAM-RNI-ALFRESC>
2019-01-24 13:54:04 +0400

    Base Procfile

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (_, res, next) => {
+   try {
+      res.send(`Hello world`);
+   } catch (error) {
+      next(error);
+   }
+});
